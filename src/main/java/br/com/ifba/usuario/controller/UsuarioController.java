71a83c4bde4709b5f886a1b6f1ef6fb1273d5f6c
Julia Freitas <Julia Freitas@DESKTOP-MC5495K>
2026-06-23 22:31:48 -0300

    feat(usuario): criar controller da entidade Usuario

---
+++ src/main/java/br/com/ifba/usuario/controller/UsuarioController.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.usuario.controller;
+
+import br.com.ifba.usuario.entity.Usuario;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author Julia Freitas
+ */
+@Controller
+public class UsuarioController implements UsuarioIController{
+    
+    @Autowired
+    private UsuarioIService usuarioService;
+
+    @Override
+    public Usuario save(Usuario usuario) throws RuntimeException {
+        return usuarioService.save(usuario);
+    }
+
+    @Override
+    public List<Usuario> findAll() throws RuntimeException {
+        return usuarioService.findAll();
+    }
+
+    @Override
+    public Usuario update(Usuario usuario) throws RuntimeException {
+        return usuarioService.update(usuario);
+    }
+
+    @Override
+    public void delete(Long id) throws RuntimeException {
+        usuarioService.delete(id);
+    }
+
+    @Override
+    public Usuario findById(Long id) throws RuntimeException {
+        return usuarioService.findById(id);
+    }
+
+    @Override
+    public Usuario findByEmail(String email) throws RuntimeException {
+        return usuarioService.findByEmail(email);
+    }
+
+    @Override
+    public void criarLista(Long id) throws RuntimeException {
+        usuarioService.criarLista(id);
+    }
+
+   /* @Override
+    public void avaliar(Long id, Object avaliacao) throws RuntimeException {
+        usuarioService.avaliar(id, avaliacao);
+    }
+
+    @Override
+    public void exibirPerfil(Long id) throws RuntimeException {
+        usuarioService.exibirPerfil(id);
+    }
+    */
+    
+}
---
+++ src/main/java/br/com/ifba/usuario/controller/UsuarioIController.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.ifba.usuario.controller;
+
+import br.com.ifba.usuario.entity.Usuario;
+import java.util.List;
+
+/**
+ *
+ * @author Julia Freitas
+ */
+public interface UsuarioIController {
+    
+    Usuario save(Usuario usuario) throws RuntimeException;
+    List<Usuario> findAll() throws RuntimeException;
+    Usuario update(Usuario usuario) throws RuntimeException;
+    void delete(Long id) throws RuntimeException;
+    Usuario findById(Long id) throws RuntimeException;    
+    
+    Usuario findByEmail(String email) throws RuntimeException;
+    
+    // Métodos de Negócio específicos 
+    void criarLista(Long id) throws RuntimeException;
+    //void avaliar(Long id, Avaliacao avaliacao) throws RuntimeException; 
+    //void exibirPerfil(Long id) throws RuntimeException;
+    
+}
