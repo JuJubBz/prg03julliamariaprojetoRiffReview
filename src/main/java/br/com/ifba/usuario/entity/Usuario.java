1207ad33756837485c74e4bae9ab80015326a8e1
Julia Freitas <Julia Freitas@DESKTOP-MC5495K>
2026-07-19 19:47:16 -0300

    feat(view): construir design da tela principal de avaliações

--- src/main/java/br/com/ifba/usuario/entity/Usuario.java
+++ src/main/java/br/com/ifba/usuario/entity/Usuario.java
@@ -4,11 +4,14 @@
  */
 package br.com.ifba.usuario.entity;
 
+import br.com.ifba.avaliacao.entity.Avaliacao;
 import br.com.ifba.infrastructure.entity.PersistenceEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.io.Serializable;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -36,9 +39,7 @@ public class Usuario extends PersistenceEntity implements Serializable{
     @Column(name = "senha", nullable = false)
     private String senha;
     
-    /*implementarei dps
     @OneToMany(mappedBy = "usuario")
     private List<Avaliacao> listaAvaliacoes;
-    */
     
 }
